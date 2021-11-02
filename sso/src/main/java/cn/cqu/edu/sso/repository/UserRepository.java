2d725634db283a09f3ff3880f7c3abdb19ecd093
卓钊 <18350120264@163.com>
2021-11-02 20:17:23 +0800

    ···········

--- sso/src/main/java/cn/cqu/edu/sso/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.cqu.edu.sso.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import cn.cqu.edu.sso.domain.User;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
