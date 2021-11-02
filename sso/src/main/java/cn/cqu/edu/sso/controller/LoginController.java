1e3c0ba57731376f2154d0c63de235a967adf59f
卓钊 <18350120264@163.com>
2021-11-02 17:09:58 +0800

    mongodb数据库

--- sso/src/main/java/cn/cqu/edu/sso/controller/LoginController.java
+++ sso/src/main/java/cn/cqu/edu/sso/controller/LoginController.java
@@ -2,7 +2,6 @@ package cn.cqu.edu.sso.controller;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +14,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 
 @RestController
 public class LoginController {
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World!";
-    }
-    //用户登录获取令牌
+    // 用户登录获取令牌
     @RequestMapping(value = "/user/login", method = RequestMethod.POST)
     public JSONObject login(@RequestParam(value = "admin", required = true) String admin,
             @RequestParam(value = "password", required = true) String password) {
@@ -27,7 +22,8 @@ public class LoginController {
         LoginService loginService = new LoginService();
         return loginService.loginPasswordCheck(user);
     }
-    //检验令牌
+
+    // 检验令牌
     @RequestMapping(value = "/user/checkToken", method = RequestMethod.POST)
     public boolean checkToken(String token, String name, String pwd) {
         LoginService loginService = new LoginService();
