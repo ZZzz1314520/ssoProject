46abbc3354c89c9e57a291e60141fbf93f620955
wuliao <1182300246@qq.com>
2021-11-03 11:58:28 +0800

    登录页数据传递测试

--- sso/src/main/java/cn/cqu/edu/sso/controller/ThymeleafController.java
+++ sso/src/main/java/cn/cqu/edu/sso/controller/ThymeleafController.java
@@ -1,21 +1,31 @@
 package cn.cqu.edu.sso.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import cn.cqu.edu.sso.util.FormData;
 
 @Controller
 public class ThymeleafController {
 
-    @RequestMapping("/index")
-    public String index(ModelMap map){
-        map.addAttribute("name","haozz");
+    @GetMapping("/index")
+    public String index() {
         return "index";
     }
 
-    @RequestMapping("/login")
-    public String login(ModelMap map){
-        map.addAttribute("name","haozz");
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("formdata", new FormData());
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(@ModelAttribute("formdata") FormData formdata) {
+        System.out.println(formdata.getUsername());
+        System.out.println(formdata.getPassword());
+        return "result";
     }
 }
