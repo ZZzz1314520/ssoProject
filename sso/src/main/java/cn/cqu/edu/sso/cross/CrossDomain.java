3d7cc9642efd6a75c95a03446d3404e314c351cf
wuliao <1182300246@qq.com>
2021-11-05 11:53:19 +0800

    舍弃WebMvcConfigurerAdapter

--- sso/src/main/java/cn/cqu/edu/sso/cross/CrossDomain.java
+++ sso/src/main/java/cn/cqu/edu/sso/cross/CrossDomain.java
@@ -1,10 +1,10 @@
 package cn.cqu.edu.sso.cross;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  
 @Configuration
-public class CrossDomain extends WebMvcConfigurerAdapter {
+public class CrossDomain implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*")
